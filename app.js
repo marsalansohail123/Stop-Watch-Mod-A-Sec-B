52b1d1643cfe5a2b4b1f7d6debd26a4c5645f2df
marsalansohail123 <arsalan.sohail1934@gmail.com>
2023-10-13 21:25:31 +0500

    Done

---
+++ app.js
@@ -0,0 +1,14 @@
+// var a = document.getElementById('main')
+// console.log(a.childNodes[3])
+
+
+// var a = document.getElementById('pera')
+// console.log(a.parentNode)
+// console.log(a.previousSibling)
+// console.log(a.nextSibling)
+
+
+var a = document.getElementById('main')
+
+console.log(a.firstChild)
+console.log(a.lastChild)
